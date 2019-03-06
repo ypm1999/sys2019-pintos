fca6773ef988d662ef81e85ab34bf053e6a99529
princeYang <lepetitprince517@sjtu.edu.cn>
2019-03-07 00:40:38 +0800

    ymt-0307.0040-timer_rewrite

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,6 +88,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int64_t sleep_ticks;                /* Ticks that the thread to sleep. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
@@ -129,6 +130,7 @@ void thread_yield (void);
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
+void thread_sleep_ticks_handler(struct thread *t, void *args UNUSED);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
